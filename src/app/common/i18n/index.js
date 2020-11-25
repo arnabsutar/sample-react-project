3a874c06c71d9d0a4b01491ca72996e586ee244c
Sutar <417810@cognizant.com>
2020-11-25 15:13:38 +0530

    -- removed comments

--- src/app/common/i18n/index.js
+++ src/app/common/i18n/index.js
@@ -4,41 +4,6 @@ import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import { enLang, frLang } from './languages';
 
-// the translations
-// (tip move them in a JSON file and import them)
-const resources = {
-  en: enLang,
-  fr: frLang,
-};
-
-// i18n
-//   // .use(Backend)
-//   .use(initReactI18next) // passes i18n down to react-i18next
-//   .init(
-//     {
-//       resources,
-//       lng: 'en',
-
-//       keySeparator: false, // we do not use keys in form messages.welcome
-
-//       interpolation: {
-//         escapeValue: false, // react already safes from xss
-//       },
-//       // backend: {
-//       //   loadPath: 'http://localhost:8080/locales/{{lng}}/{{ns}}.json',
-//       // },
-//     },
-//     // eslint-disable-next-line consistent-return
-//     (err, t) => {
-//       // eslint-disable-next-line no-console
-//       if (err) return console.error(err);
-//       // eslint-disable-next-line no-console
-//       console.log(t('welcome'));
-//       // eslint-disable-next-line no-console
-//       console.log(t('welcome', { lng: 'fr' }));
-//     },
-//   );
-
 i18n
   .use(Backend)
   .use(initReactI18next)
