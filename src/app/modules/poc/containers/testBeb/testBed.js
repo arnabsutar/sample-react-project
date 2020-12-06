140264f5635f25f55d230c29d60c0a70c02dcd01
Sutar <417810@cognizant.com>
2020-12-06 09:07:56 +0530

    -- code compiled

--- src/app/modules/poc/containers/testBeb/testBed.js
+++ src/app/modules/poc/containers/testBeb/testBed.js
@@ -1,5 +1,30 @@
+/* eslint-disable no-shadow */
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 import React from 'react';
+import { connect } from 'react-redux';
+import { applicationLoadedSuccessfully } from '../../../../common/redux/actions/commonActions';
 
-const TestBed = () => <div>THIS IS THE TEST BED</div>;
+const mapStateToProps = (state) => ({
+  applicationLoaded: state.ApplicationBaseReducer.loaded,
+});
 
-export default TestBed;
+const mapDispatchToProps = (dispatch) => ({
+  setApplicationLoaded: () => dispatch(applicationLoadedSuccessfully),
+});
+
+const TestBed = (props) => {
+  const { applicationLoadedSuccessfully, applicationLoaded } = props;
+  // applicationLoadedSuccessfully();
+  return (
+    <div>
+      THIS IS THE TEST BED - Application loaded
+      <p>
+        {JSON.stringify(props)}
+        {applicationLoaded}
+      </p>
+    </div>
+  );
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TestBed);
