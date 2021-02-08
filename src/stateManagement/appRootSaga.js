379a2e5302147749bc6c9eb82c83baaa590d4ba9
arnabsutar <417810@cognizant.com>
2021-02-08 18:04:56 +0530

    interim

--- src/stateManagement/appRootSaga.js
+++ src/stateManagement/appRootSaga.js
@@ -1,7 +1,7 @@
 import { all } from 'redux-saga/effects';
 import applicationBaseWatcher from '../app/common/redux/sagas/applicationBaseWatcher';
-import { agency1Watcher } from '../app/modules/agency1';
+
 
 export default function* RootSaga() {
-  yield all([applicationBaseWatcher(),);, , agency1Watcher]);
+  yield all([applicationBaseWatcher()]);
 }
