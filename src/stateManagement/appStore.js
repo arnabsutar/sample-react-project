a33fac1093e1a3e8e66eccc515dc2a9dbbe9c2e4
arnabsutar <417810@cognizant.com>
2021-02-08 18:17:03 +0530

    test

--- src/stateManagement/appStore.js
+++ src/stateManagement/appStore.js
@@ -2,8 +2,8 @@ import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-import RootReducer from './rootReducer';
-import RootSaga from './rootSaga';
+import RootReducer from './appRootReducer';
+import RootSaga from './appRootSaga';
 
 const sagaMiddleware = createSagaMiddleware();
 
